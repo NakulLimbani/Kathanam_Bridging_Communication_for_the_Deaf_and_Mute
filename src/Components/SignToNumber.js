ebe8b280510d70f73071835cb4eaa927005008e2
Nakul Limbani <113363295+NakulLimbani@users.noreply.github.com>
2025-03-29 13:31:45 +0530

    Update SignToNumber.js

--- src/Components/SignToNumber.js
+++ src/Components/SignToNumber.js
@@ -26,7 +26,7 @@ const SignToNumber = () => {
     formData.append("file", selectedImage);
 
     try {
-      const response = await fetch("http://127.0.0.1:5000/predict", {
+      const response = await fetch("https://number-service-server.onrender.com/predict", {
         method: "POST",
         body: formData,
       });
